2d9bea8a83ef6d2d91b825475269fbc5b2e76f7b
Mehul Pillai <mehulpillai30@gmail.com>
2020-12-11 19:24:05 -0500

    Adds mongoose

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+mongoose.connect("mongodb://localhost:27017/express", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+    });
+});
