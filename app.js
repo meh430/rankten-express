26721b07417c44b89057b0e3f5a526b380558bde
Mehul Pillai <mehulpillai30@gmail.com>
2020-12-17 17:57:35 -0500

    Testing out mysql

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const sql = require("./models/database");
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
--- models/database.js
+++ models/database.js
@@ -2,10 +2,16 @@ const config = require("../config").config;
 const mysql = require('mysql');
 const connection = mysql.createConnection(config);
 
-connection.connect(error => {
-    if (error) throw error;
+connection.connect(err => {
+    if (err) throw console.log(err);
 
     console.log("Connected");
+    connection.query("CREATE DATABASE IF NOT EXISTS rank_ten;", (err) => console.log(err ? err : "1"));
+    connection.query("USE rank_ten", (err) => console.log(err ? err : "2"));
+    connection.query("CREATE TABLE IF NOT EXISTS employee_table(id int NOT NULL AUTO_INCREMENT, name varchar(45) NOT NULL, occupation varchar(35) NOT NULL, age int NOT NULL, PRIMARY KEY (id));", (err) => {
+        console.log(err);
+    });
 })
+
 
 module.exports = connection;
