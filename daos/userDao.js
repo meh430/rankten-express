d0142e39ff7ba3f21b41e99e28ee45f3e4014f4c
Mehul Pillai <mehulpillai30@gmail.com>
2020-12-20 11:11:38 -0500

    adds updateUser

--- daos/userDao.js
+++ daos/userDao.js
@@ -22,3 +22,27 @@ async function createUser(connection, user) {
     return res.insertId;
 }
 
+async function updateUser(connection, userId, user) {
+    delete user.userId;
+    delete user.dateCreated;
+    delete user.rankPoints;
+
+    if ('password' in user) {
+        user.password = bcrypt.hashSync(user.password);
+    }
+
+    const res = await sql.query(connection, queries.updateUserQuery(userId, user));
+    console.log(res);
+
+    if (res.affectedRows === 0) {
+        throw errors.notFoundError();
+    }
+}
+
+async function deleteUser(connection, user) {
+
+}
+
+async function getUser(connection, user, private) {
+    return "TODO";
+}
